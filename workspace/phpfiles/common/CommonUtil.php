9d0fc800921042de0e7ffcdbeb7c69cfd176d3a2
yutaka Yamashiro <ysy0217@gmail.com>
2011-06-18 18:27:42 +0900

    add common and exclude.php and modify build.xml for to use

---
+++ workspace/phpfiles/common/CommonUtil.php
@@ -0,0 +1,14 @@
+<?php
+class CommonUtil {
+  function hoge1() {}
+  function hoge2() {}
+  function hoge3() {}
+  function hoge4() {}
+  function hoge5() {}
+  function hoge6() {}
+  function hoge7() {}
+  function hoge8() {}
+  function hoge9() {}
+  function hoge10() {}
+  function hoge11() {}
+}
---
+++ workspace/phpfiles/exclude.php
@@ -0,0 +1,6 @@
+<?php
+
+class Exclude {
+  function hoge($notUsedParam) {
+  }
+}
